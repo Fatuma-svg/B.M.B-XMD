9a5b4f96f53584bcce8496ebef42d5363081b56e
Fatuma-svg <fally6727@gmail.com>
2025-04-24 13:49:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "B.m.b-Xmd~dTknhIKA#VYmAcveaHvyh4dmIe94W5Ug40gqXFj8vB1x8XihiKxY",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
